0325c716b48766292becbcb638a2aaf0d6800a2c
GiorgiDatashvili <duche33@gmail.com>
2024-11-20 18:13:36 +0400

    fifth commit

---
+++ GOA/level.22/homework/main.py
@@ -0,0 +1,19 @@
+list_of_names = ["გიორგი", "დავითი", "ლუკა", "დავითი"]
+print("დავითი სიაში",list_of_names.count("დავითი"),"-ჯერ არის")
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+nums.reverse()
+print(nums)
+
+list_of_numbers = [1, 2, 3]
+new_list = list_of_numbers * len(list_of_numbers)
+print(new_list)
+
+insert_arr = ["javascript","java", "python", "c++"]
+insert_arr.insert(3, "c#")
+print(insert_arr)
+
+numbers = [1 ,2 ,3 ,4, 5 ,6, 7]
+numbers.remove(1)
+print(numbers)
+
