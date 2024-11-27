f8a79acd73e0492711fda6aaf3b3d70579dabaef
GiorgiDatashvili <duche33@gmail.com>
2024-11-27 13:19:44 +0400

    sixth commit

---
+++ GOA/level.24/classwork/main.py
@@ -0,0 +1,9 @@
+message = "witeli vashli  {0} {1}".format("gagorda","50m")
+print(message)
+
+
+saxeli = "ixvis tolma ar aris batis tolma"
+split_saxeli = saxeli.split()
+split_saxeli = " ".join(split_saxeli)
+print(split_saxeli)
+
---
+++ GOA/level.24/homework/main.py
@@ -0,0 +1,39 @@
+# 1
+list ="gamarjoba rogor xar"
+new_list = []
+for i in list:
+    if i == " ":
+        new_list.append(list)
+        break
+print(new_list)
+
+# 2
+x = ["hello" , "hi" , "gamarjoba"]
+gamnawilebeli = ","
+shedegi = " "
+for i in x:
+        shedegi += i + ","
+print(shedegi)
+
+# 3
+
+text = "me mikvars pica"
+for i in text:
+     if i == "p":
+        print("me mikvars xinkali")
+
+# 5
+word_1 = input("აირჩიე ნებისმიერი სიტყვა:")
+word_2 = input("აირჩიე მეორე სიტყვა:")
+word_3 = input("აირჩიეთ მესამე სიტყვა:")
+list = [ ]
+arr = [ ]
+list = arr.append(word_1)
+list = arr.append(word_2)
+list = arr.append(word_3)
+print(arr)
+          
+
+
+
+
