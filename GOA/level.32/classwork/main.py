0026c04f8da7d068714251b7f9badce908f6c122
GiorgiDatashvili <duche33@gmail.com>
2024-12-24 17:21:25 +0400

    tenth commit

---
+++ GOA/level.32/classwork/main.py
@@ -0,0 +1,11 @@
+def opposite(number):
+    return -number
+#https://www.codewars.com/kata/56dec885c54a926dcd001095/train/python
+
+def repeat_str(repeat, string):
+    return repeat * string
+#https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/python
+
+def greet():
+    return "hello world!"
+#https://www.codewars.com/kata/523b4ff7adca849afe000035/train/python
